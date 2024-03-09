adcf73d089681bf51be6eacbb4dba5f33906825c
muhtadeet <sheikh.taron@northsouth.edu>
2024-03-09 23:43:32 +0600

    updated photos

--- components/Card.tsx
+++ components/Card.tsx
@@ -30,48 +30,6 @@ export default function WeatherCard({
       shadow="sm"
     >
       <CardBody>
-        {/* <div className="grid grid-cols-6 md:grid-cols-12 gap-6 md:gap-4 items-center justify-center">
-          <div className="relative col-span-6 md:col-span-4">
-            <Image
-              alt="Album cover"
-              className="object-cover"
-              height={200}
-              width={200}
-              src="/cloudy.png"
-            />
-          </div>
-
-          <div className="flex flex-col col-span-6 md:col-span-8">
-            <div className="flex">
-              <div className="flex flex-col">
-                <div className="flex flex-row justify-center">
-                  <p className="font-semibold text-3xl text-foreground/90">{name}</p>
-                  <p className="text-3xl text-foreground/80">{temp} °C</p>
-                </div>
-
-                <h1 className="text-large font-medium mt-2">Frontend Radio</h1>
-              </div>
-            </div>
-
-            <div className="flex flex-col mt-3 gap-1">
-              <Slider
-                aria-label="Music progress"
-                classNames={{
-                  track: "bg-default-500/30",
-                  thumb: "w-2 h-2 after:w-2 after:h-2 after:bg-foreground",
-                }}
-                color="foreground"
-                defaultValue={33}
-                size="sm"
-              />
-              <div className="flex justify-between">
-                <p className="text-small">1:23</p>
-                <p className="text-small text-foreground/50">4:32</p>
-              </div>
-            </div>
-          </div>
-        </div> */}
-
         <div className="flex items-start justify-around gap-4">
           <div className="flex flex-col justify-around gap-2">
             <h1 className="text-6xl font-black">{temp}°</h1>
@@ -82,7 +40,26 @@ export default function WeatherCard({
             </p>
             <p className="text-sm">Feels Like {feelsLike}°</p>
           </div>
-          <Image alt="Status" src="/cloudy.png" width={100} height={100} />
+          <Image
+            alt="Status"
+            src={
+              status === "Clear"
+                ? "/clear.png"
+                : status === "Clouds"
+                ? "/cloudy.png"
+                : status === "Rain"
+                ? "/rain.png"
+                : status === "Haze"
+                ? "/haze.png"
+                : status === "Thunderstorm"
+                ? "/tstorm.png"
+                : status === "Snow"
+                ? "/snow.png"
+                : "/cloudy.png"
+            }
+            width={100}
+            height={100}
+          />
         </div>
       </CardBody>
       <CardFooter className="flex items-center justify-around">
